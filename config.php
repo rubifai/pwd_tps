5a1efba7c18a402dac7f13a67c2388bd1aebc0e9
rubifai <francisco.rubilar@est.fi.uncoma.edu.ar>
2023-05-06 00:21:38 -0300

    estructura empezada

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+$GLOBALS["ROOT"] = $_SERVER["DOCUMENT_ROOT"] . "/" ;
+
+?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INICIO</title>
+</head>
+
+<body>
+    <H1>INDEX PRIMER COMMIT</H1>
+    <?php
+
+    echo "hola mundo php server /n";
+    for ($i = 0; $i < 10; $i++) {
+        # code...
+        echo $i . "<br/>";
+        
+    }
+   
+
+    
+    ?>
+
+
+<script>
+    function saluda(){
+        alert("alfin delfin");
+    }
+    saluda();
+</script>
+</body>
+
+</html>
+
